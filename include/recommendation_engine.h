0d10c7057be62c8f8cb51a336477e0edf5872644
Minjoo Kang <kmjrina03@gmail.com>
2025-11-19 15:13:54 +0900

    Update recommendation_engine.h

--- include/recommendation_engine.h
+++ include/recommendation_engine.h
@@ -1,8 +1,12 @@
-
 #pragma once
 #include <vector>
 #include "event_repository.h"
 #include "geo.h"
+
+struct RecommendedEvent {
+    CulturalEvent event;
+    double distanceKm;
+};
 
 class RecommendationEngine {
 private:
@@ -11,6 +15,6 @@ private:
 public:
     RecommendationEngine(EventRepository& repo);
 
-    std::vector<CulturalEvent> recommendByLocation(const GeoLocation& userLoc, double radius);
-    std::vector<CulturalEvent> recommendByDistrict(const std::string& districtName);
+    std::vector<RecommendedEvent> recommendByLocation(const GeoLocation& userLoc, double radius);
+    std::vector<CulturalEvent>    recommendByDistrict(const std::string& districtName);
 };
